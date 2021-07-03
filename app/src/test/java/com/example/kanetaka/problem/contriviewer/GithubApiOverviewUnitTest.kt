f2b566442afb6c00a0518d03aa9587ba526b9a84
cch-robo <robo@cch-lab.com>
2021-07-03 14:41:10 +0900

    JUnitでのコルーチンテストに問題があるため改修 （テスト用 org.jetbrains.kotlinx:kotlinx-coroutines-tes は使っていません）

--- app/src/test/java/com/example/kanetaka/problem/contriviewer/GithubApiOverviewUnitTest.kt
+++ app/src/test/java/com/example/kanetaka/problem/contriviewer/GithubApiOverviewUnitTest.kt
@@ -1,28 +1,47 @@
 package com.example.kanetaka.problem.contriviewer
 
+import com.example.kanetaka.problem.contriviewer.infra.githubapi.overview.ContributorsModel
 import com.example.kanetaka.problem.contriviewer.infra.githubapi.overview.OverviewService
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 
 /**
  * OverviewService 動作確認（ネットワークが機能するときしかテストできません）
  */
 class GithubApiOverviewUnitTest {
+    private lateinit var contributors: List<ContributorsModel>
+
+    @Before
+    fun setUp() {
+        // テスト時には、コルーチンから
+        // プラットフォーム Mainディスパッチャーが使用できないので単純な解決策で対応
+        //
+        // Kotlin の Unit Test 用ライブラリを試したが、複雑になるため今回は利用していません。
+        // kotlinx.coroutines/kotlinx-coroutines-test/
+        // https://github.com/Kotlin/kotlinx.coroutines/tree/master/kotlinx-coroutines-test
+
+        setupContributors()
+        Thread.sleep(5000)
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+
     /**
      * OverviewService がコントリビュータ一覧を返すかチェック
      */
     @Test
     fun can_fetch_github_api_contributors() {
-        val scope = CoroutineScope(Job() + Dispatchers.IO)
-        scope.launch {
-            val response = OverviewService.retrofitService.getContributors()
-            assertTrue(response.isNotEmpty())
-        }
+        assertTrue(contributors.isNotEmpty())
     }
 
     /**
@@ -30,10 +49,17 @@ class GithubApiOverviewUnitTest {
      */
     @Test
     fun fetch_github_api_contributors_isComplete() {
+        assertEquals(contributors.size, 30)
+    }
+
+    /**
+     * OverviewService からコントリビュータ一覧を取得する。
+     */
+    private fun setupContributors() {
         val scope = CoroutineScope(Job() + Dispatchers.IO)
         scope.launch {
             val response = OverviewService.retrofitService.getContributors()
-            assertEquals(response.size, 30)
+            contributors = response
         }
     }
 }
