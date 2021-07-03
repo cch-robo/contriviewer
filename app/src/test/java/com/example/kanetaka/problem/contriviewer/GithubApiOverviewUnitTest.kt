438bb1edc475506b25086fc6cf787ef7aa66ff7f
cch-robo <robo@cch-lab.com>
2021-07-03 11:54:42 +0900

    GitHub API コントリビュータ一覧取得サービスの動作確認 IOスレッドで通信させるためコルーチンライブラリを追加しています。

---
+++ app/src/test/java/com/example/kanetaka/problem/contriviewer/GithubApiOverviewUnitTest.kt
@@ -0,0 +1,35 @@
+package com.example.kanetaka.problem.contriviewer
+
+import com.example.kanetaka.problem.contriviewer.infra.githubapi.overview.OverviewService
+import kotlinx.coroutines.*
+import org.junit.Test
+import org.junit.Assert.*
+
+/**
+ * OverviewService 動作確認（ネットワークが機能するときしかテストできません）
+ */
+class GithubApiOverviewUnitTest {
+    /**
+     * OverviewService がコントリビュータ一覧を返すかチェック
+     */
+    @Test
+    fun can_fetch_github_api_contributors() {
+        val scope = CoroutineScope(Job() + Dispatchers.IO)
+        scope.launch {
+            val response = OverviewService.retrofitService.getContributors()
+            assertTrue(response.isNotEmpty())
+        }
+    }
+
+    /**
+     * GitHub API のコントリビュータ一覧のデフォルト仕様、30件分を取得するかチェック
+     */
+    @Test
+    fun fetch_github_api_contributors_isComplete() {
+        val scope = CoroutineScope(Job() + Dispatchers.IO)
+        scope.launch {
+            val response = OverviewService.retrofitService.getContributors()
+            assertEquals(response.size, 30)
+        }
+    }
+}
