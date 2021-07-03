8d0886f10f6b561b18c5e0f0b5acb475b9318b09
cch-robo <robo@cch-lab.com>
2021-07-03 22:34:46 +0900

    コントリビュータ詳細へ遷移機能（Argument/パラメータを伴う）を追加

--- app/src/main/java/com/example/kanetaka/problem/contriviewer/page/overview/OverviewContributorsAdapter.kt
+++ app/src/main/java/com/example/kanetaka/problem/contriviewer/page/overview/OverviewContributorsAdapter.kt
@@ -2,22 +2,25 @@ package com.example.kanetaka.problem.contriviewer.page.overview
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.kanetaka.problem.contriviewer.R
 import com.example.kanetaka.problem.contriviewer.databinding.OverviewItemBinding
+import com.example.kanetaka.problem.contriviewer.page.detail.DetailContributorArguments
 import com.example.kanetaka.problem.contriviewer.util.Utilities.debugLog
 
 data class OverviewContributor(
     val id: Long,
     val name: String,
     val iconUrl: String,
-    val contributions: Long
+    val contributions: Long,
+    val contributorUrl: String
 )
 
-class OverviewContributorsAdapter :
+class OverviewContributorsAdapter(private val fragment: OverviewFragment) :
     ListAdapter<OverviewContributor, RecyclerView.ViewHolder>(OverviewContributorDiffCallback()) {
 
     init {
@@ -27,7 +30,10 @@ class OverviewContributorsAdapter :
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContributorViewHolder {
         debugLog("OverviewContributorsAdapter#onCreateViewHolder [${viewType}]")
         val layoutInflater = LayoutInflater.from(parent.context)
-        return ContributorViewHolder(OverviewItemBinding.inflate(layoutInflater, parent, false))
+        return ContributorViewHolder(
+            OverviewItemBinding.inflate(layoutInflater, parent, false),
+            fragment
+        )
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
@@ -35,17 +41,28 @@ class OverviewContributorsAdapter :
         (holder as ContributorViewHolder).bind(item)
     }
 
-    class ContributorViewHolder(private val binding: OverviewItemBinding) :
+    class ContributorViewHolder(
+        private val binding: OverviewItemBinding,
+        private val fragment: OverviewFragment
+    ) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(item: OverviewContributor) {
             debugLog("ContributorViewHolder#bind [${item.name}]")
             binding.contributorName.text = item.name
             binding.contributorContribution.text = item.contributions.toString()
+
             Glide.with(binding.root.context)
                 .load(item.iconUrl)
                 .placeholder(R.drawable.loading_animation)
                 .error(R.drawable.ic_connection_error)
                 .into(binding.contributorIcon)
+
+            binding.contributorItem.setOnClickListener {
+                val action = OverviewFragmentDirections.actionOverviewToDetail(
+                    DetailContributorArguments(item.id, item.name, item.contributorUrl)
+                )
+                fragment.findNavController().navigate(action)
+            }
         }
     }
 
