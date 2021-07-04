babbb83c6d27639620c9dbcd75534b26d60e62e6
cch-robo <robo@cch-lab.com>
2021-07-04 19:43:44 +0900

    コントリビュータ詳細画面の View表示項目モデルを新規追加

---
+++ app/src/main/java/com/example/kanetaka/problem/contriviewer/page/detail/DetailContributor.kt
@@ -0,0 +1,17 @@
+package com.example.kanetaka.problem.contriviewer.page.detail
+
+data class DetailContributor(
+    val iconUrl: String,
+    val login: String,
+    val name: String?,
+    val bio: String?,
+    val company: String?,
+    val location: String?,
+    val email: String?,
+    val blog: String,
+    val twitter_username: String?,
+    val followers: Long,
+    val following: Long,
+    val public_repos: Long,
+    val public_gists: Long
+)
